e58e8a75b397561bbf8bce12597e2d41ec622a0a
Liangtd <Liangtd0301@qq.com>
2024-10-09 16:17:54 +0800

    update🆙: 切换首页demo，暂时移除echarts demo页面

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,10 +1,15 @@
+<!--
+ * @Author: liangtd
+ * @Date: 2024-09-27 16:55:58
+ * @LastEditors: liangtd
+ * @LastEditTime: 2024-10-09 16:14:53
+ * @Description:
+-->
 <template>
-  <!-- <DemoIndex /> -->
-   <Demo />
+  <DemoIndex />
 </template>
 <script lang="ts" setup>
 import DemoIndex from '../demo.vue'
-import Demo from '../demo1.vue'
 </script>
 
 <style lang="scss" scoped></style>
